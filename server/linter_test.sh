c711a82d5125ed871c405cb6bc75ac1ee95b87ac
Pipin Fitriadi <pipinfitriadi@gmail.com>
2020-03-21 17:19:03 +0700

    Perbaikan Gitlab CI/CD Linter Test

--- server/linter_test.sh
+++ server/linter_test.sh
@@ -62,5 +62,5 @@ last_commit_hash=$(git log -n 1 --pretty=format:%H)
 file_last_commit_hash=$(git log -n 1 --pretty=format:%H -- *.py)
 
 if [ $last_commit_hash == $file_last_commit_hash ]; then
-    docker-compose -f server/docker-compose/linter_test.yml up
+    docker-compose -f server/docker-compose/linter_test.yml run --rm linter_test
 fi
