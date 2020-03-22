82c8515f70c80cc1a03d359def35e3fc5c29a5cb
Pipin Fitriadi <pipinfitriadi@gmail.com>
2020-03-22 10:24:58 +0700

    Rapih rapih

--- server/docker_image.sh
+++ server/docker_image.sh
@@ -57,6 +57,7 @@
 # https://forum.gitlab.com/t/better-way-for-multiline-ssh-command/23420
 set -e
 filename='server/docker-compose/Dockerfile'
+
 # How do I find the most recent git commit that modified a file?
 # https://stackoverflow.com/questions/4784575/how-do-i-find-the-most-recent-git-commit-that-modified-a-file
 last_commit_hash=$(git log -n 1 --pretty=format:%H)
@@ -66,6 +67,7 @@ if [ $last_commit_hash == $file_last_commit_hash ]; then
     # Docker Registry login and docker CI template
     # https://gitlab.com/gitlab-org/gitlab-runner/issues/2861
     echo $CI_REGISTRY_PASSWORD | docker login -u $CI_REGISTRY_USER $CI_REGISTRY --password-stdin
+
     docker-compose -f server/docker-compose/docker_image.yml pull || true
     docker-compose -f server/docker-compose/docker_image.yml build
     docker-compose -f server/docker-compose/docker_image.yml push
