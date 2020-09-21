7bc60437183520dce052e9d359d4aa33471528b7
Pipin Fitriadi <pipinfitriadi@gmail.com>
2020-09-21 16:57:58 +0700

    update versi

--- setup.py
+++ setup.py
@@ -84,7 +84,7 @@ setup(
             'server/nginx/*'
         ]
     },
-    version='0.9.8',
+    version='0.9.9',
     description=(description := "VOXROW's library"),
     long_description=description,
     author='Pipin Fitriadi',
