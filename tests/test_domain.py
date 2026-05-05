11619b513ee105335c253fa27f89ae7f0edf8f79
Pipin Fitriadi <pipin.fitriadi@alto.id>
2026-05-05 15:14:43 +0700

    fix: code coverage for domain

--- tests/test_domain.py
+++ tests/test_domain.py
@@ -7,6 +7,7 @@
 # Written by Pipin Fitriadi <pipinfitriadi@gmail.com>, 5 May 2026
 
 import gzip
+from datetime import date
 
 import pytest
 
@@ -14,23 +15,30 @@ from voxrow.core.domain import domain_services
 from voxrow.core.domain.value_objects import ENCODING
 
 # Constants
-TEST_DATA_FOR_GZIP: tuple = (1, 2, 3)
-TEST_DATA_FOR_GZIP_BYTES: bytes = str(TEST_DATA_FOR_GZIP).encode(ENCODING)
+TEST_DATA: tuple = (1, 2, 3)
+TEST_DATA_BYTES: bytes = str(TEST_DATA).encode(ENCODING)
+TEST_DATA_JSON: str = "[1, 2, 3]"
 
 
 @pytest.fixture
 def fake_gzip() -> bytes:
-    return gzip.compress(TEST_DATA_FOR_GZIP_BYTES, compresslevel=9)
+    return gzip.compress(TEST_DATA_BYTES, compresslevel=9)
 
 
 class TestDomainServices:
-    def test_compress_to_gzip(self, fake_gzip: bytes) -> None:
-        assert domain_services.compress_to_gzip(*(TEST_DATA_FOR_GZIP,)) == fake_gzip
+    def test_functions(self) -> None:
+        assert isinstance(domain_services.today(), date)
+        assert domain_services.now().tzinfo is not None
 
-    def test_decompress_from_gzip(self, fake_gzip: bytes) -> None:
+    def test_gzip(self, fake_gzip: bytes) -> None:
+        assert domain_services.compress_to_gzip(*(TEST_DATA,)) == fake_gzip
         assert (
             domain_services.decompress_from_gzip(
                 fake_gzip,
             )
-            == TEST_DATA_FOR_GZIP_BYTES
+            == TEST_DATA_BYTES
         )
+
+    def test_json(self) -> None:
+        assert domain_services.dumps_to_json(*(TEST_DATA,)) == TEST_DATA_JSON
+        assert tuple(domain_services.loads_from_json(TEST_DATA_JSON)) == TEST_DATA
