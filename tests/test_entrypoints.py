1551cc46638d20331cb2a4f3833a01fde3a637fb
Pipin Fitriadi <pipin.fitriadi@alto.id>
2026-05-22 16:34:36 +0700

    fix: Implement logging entrypoints

--- tests/test_entrypoints.py
+++ tests/test_entrypoints.py
@@ -6,6 +6,7 @@
 # Proprietary and confidential
 # Written by Pipin Fitriadi <pipinfitriadi@gmail.com>, 21 May 2026
 
+import logging
 from pathlib import Path
 
 import pytest
@@ -14,7 +15,7 @@ from typer import Context, Typer
 from typer.testing import CliRunner, Result
 
 from voxrow.core.domain import value_objects
-from voxrow.core.entrypoints import typer
+from voxrow.core.entrypoints import set_logging_config, typer
 
 
 @pytest.fixture
@@ -22,14 +23,22 @@ def fake_env_file(tmp_path: Path) -> FilePath:
     return tmp_path / ".env"
 
 
+@pytest.fixture
+def fake_log_msg() -> str:
+    return "Test.."
+
+
 class TestTyper:
     app: Typer
     runner: CliRunner
 
     @pytest.fixture(autouse=True)
-    def setup(self) -> None:
+    def setup(self, fake_log_msg: str) -> None:
+        set_logging_config()
+
         self.runner = CliRunner()
         self.app = Typer()
+        logger: logging.Logger = logging.getLogger(__name__)
 
         @self.app.callback()
         def callback(context: Context, env_file: FilePath) -> None:
@@ -37,12 +46,21 @@ class TestTyper:
 
         @self.app.command()
         @typer.inject_settings
-        def command(*, settings: value_objects.Settings) -> None: ...
+        def command(*, settings: value_objects.Settings) -> None:  # noqa: ARG001
+            logger.info(fake_log_msg)
 
-    def test_command(self, fake_env_file: FilePath) -> None:
+    def test_command(
+        self,
+        caplog: pytest.LogCaptureFixture,
+        fake_env_file: FilePath,
+        fake_log_msg: str,
+    ) -> None:
         result: Result = self.runner.invoke(
             self.app,
             [fake_env_file.as_posix(), "command"],
         )
+        record: logging.LogRecord = caplog.records[0]
 
+        assert record.levelno == logging.INFO
+        assert record.msg == fake_log_msg
         assert result.exit_code == 0
--- voxrow/core/entrypoints/__init__.py
+++ voxrow/core/entrypoints/__init__.py
@@ -5,3 +5,68 @@
 # Unauthorized copying of this file, via any medium is strictly prohibited
 # Proprietary and confidential
 # Written by Pipin Fitriadi <pipinfitriadi@gmail.com>, 21 May 2026
+
+import logging
+from datetime import timedelta
+
+from pydantic import validate_call
+from rich.console import ConsoleRenderable
+from rich.logging import RichHandler
+from rich.table import Table
+from rich.text import Text
+from rich.traceback import Traceback
+
+from ..domain import value_objects
+
+
+class DeltaRichHandler(RichHandler):
+    @validate_call
+    def __init__(self, *args, **kwargs) -> None:  # noqa: ANN002,ANN003
+        super().__init__(*args, **kwargs)
+        self._last: dict = {}
+
+    @validate_call(config=value_objects.CONFIG_DICT)
+    def render(
+        self,
+        record: logging.LogRecord,
+        traceback: Traceback | None,
+        message_renderable: ConsoleRenderable,
+    ) -> ConsoleRenderable:
+        key: str = record.name
+        now: float = record.created
+        last: float | None = self._last.get(key)
+        self._last[key] = now
+        table: Table = Table.grid(padding=1, expand=True)
+
+        table.add_column()
+        table.add_column(justify="right")
+        table.add_row(
+            message_renderable,  # message log
+            Text(
+                f"+{timedelta(seconds=0.0 if last is None else now - last)}",
+                style="dim",
+            ),  # delta time
+        )
+
+        return super().render(
+            record=record,
+            traceback=traceback,
+            message_renderable=table,
+        )
+
+
+@validate_call
+def set_logging_config() -> None:
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(message)s",
+        handlers=[
+            DeltaRichHandler(
+                show_path=False,
+                markup=True,
+                rich_tracebacks=True,
+                log_time_format="[%Y-%m-%d %H:%M:%S]",
+            )
+        ],
+        force=True,
+    )
