b1ca102e547b86a08fe265e3a54ccd17df603c31
Pipin Fitriadi <pipin.fitriadi@alto.id>
2026-07-24 16:31:25 +0700

    fix: Implement AesGcmEncryptionDataAdapter.extract

--- voxrow/core/adapters/data/cryptography.py
+++ voxrow/core/adapters/data/cryptography.py
@@ -6,13 +6,14 @@
 # Proprietary and confidential
 # Written by Pipin Fitriadi <pipinfitriadi@gmail.com>, 24 July 2026
 
+from io import BytesIO
 from os import urandom
 
 from pydantic import validate_call
 from pydantic.dataclasses import dataclass
 
 from ...domain import value_objects
-from ..utils import cryptography
+from ..utils.cryptography import cryptography
 from . import AbstractDataPort
 
 
@@ -23,7 +24,31 @@ class AesGcmEncryptionDataAdapter(
 ):
     @validate_call(config=value_objects.CONFIG_DICT, validate_return=True)
     def extract(self, *, source: value_objects.EncryptionSource) -> value_objects.Data:
-        pass
+        counter: int = 0
+        nonce: bytes | str | any = source.read(self.urandom_size)
+        data: bytes = BytesIO()
+
+        while True:
+            bytes_length: bytes | str | any = source.read(self.to_bytes_length)
+
+            if not bytes_length:
+                break
+
+            data.write(
+                self.aesgcm.decrypt(
+                    nonce[: self.nonce_length] + counter.to_bytes(
+                        self.to_bytes_length, self.byteorder
+                    ),  # Chunk Nonce: 12 == 8 + 4,
+                    source.read(
+                        int.from_bytes(bytes_length, self.byteorder)  # Encrypted length
+                    ),  # Encrypted
+                    None,
+                )  # Plain text
+            )
+
+            counter += 1
+
+        return data
 
     @validate_call(config=value_objects.CONFIG_DICT, validate_return=True)
     async def load(
@@ -32,9 +57,7 @@ class AesGcmEncryptionDataAdapter(
         *,
         destination: value_objects.EncryptionDestination,
     ) -> value_objects.ResourceLocation:
-        if isinstance(data, value_objects.ReadableStream) and isinstance(
-            destination, value_objects.WritableStream
-        ):
+        if isinstance(data, value_objects.ReadableStream):
             counter: int = 0
             nonce: bytes = urandom(self.urandom_size)
 
