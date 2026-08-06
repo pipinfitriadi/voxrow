1be1dcb6ca5b2fadb014797f3bb272a002729ead
Pipin Fitriadi <pipin.fitriadi@alto.id>
2026-08-06 12:40:01 +0700

    fix: Reorder value_objects

--- voxrow/core/domain/value_objects.py
+++ voxrow/core/domain/value_objects.py
@@ -43,27 +43,7 @@ TIME_ZONE: ZoneInfo = ZoneInfo("Asia/Jakarta")
 Param: ParamSpec = ParamSpec("Param")
 
 
-class CaseInsensitiveStrEnum(StrEnum):
-    @classmethod
-    @validate_call(validate_return=True)
-    def _missing_(cls, value: Any) -> StrEnum | None:  # noqa: ANN401
-        if isinstance(value, str):
-            for member in cls:
-                if member.value == value.lower():
-                    return member
-
-        return None
-
-
-class LogLevel(IntEnum):
-    CRITICAL = logging.CRITICAL
-    FATAL = logging.FATAL
-    ERROR = logging.ERROR
-    WARNING = logging.WARNING
-    WARN = logging.WARN  # noqa: LOG009
-    INFO = logging.INFO
-    DEBUG = logging.DEBUG
-    NOTSET = logging.NOTSET
+# ====================================== Settings ======================================
 
 
 class Settings(BaseSettings):
@@ -75,33 +55,16 @@ class Settings(BaseSettings):
     )
 
 
-type DatabaseType = PostgresDsn | AnyUrl
+# =================================== Data & Resource ==================================
+
+
+type Row = dict[Any, Any]
 
 
 @runtime_checkable
 class ReadableStream(Protocol):
     @validate_call(validate_return=True)
     def read(self, size: int = -1) -> bytes | str | Any: ...  # noqa: ANN401
-
-
-@runtime_checkable
-class WritableStream(Protocol):
-    @validate_call(validate_return=True)
-    def write(self, data: bytes) -> int: ...
-
-
-class Domain:
-    pass
-
-
-@dataclass(frozen=True)
-class Table:
-    name: str
-    schema: str
-
-
-type ResourceLocation = Table | HttpUrl | AnyUrl | Path | WritableStream | Any
-type Row = dict[Any, Any]
 
 
 class Rows(Iterator[Row]):
@@ -119,31 +82,28 @@ class Rows(Iterator[Row]):
         )
 
 
+@dataclass(frozen=True)
+class Table:
+    name: str
+    schema: str
+
+
+@runtime_checkable
+class WritableStream(Protocol):
+    @validate_call(validate_return=True)
+    def write(self, data: bytes) -> int: ...
+
+
+# --------------------------------------------------------------------------------------
+
+
 type Data = Rows | bytes | str | ReadableStream | Any
+type ResourceLocation = Table | HttpUrl | AnyUrl | Path | WritableStream | Any
 
 
-class ContentEncoding(StrEnum):
-    gzip = "gzip"
+# ======================================================================================
 
-
-class ContentType(StrEnum):
-    html = "text/html"
-    json = "application/json"
-    svg = "image/svg+xml"
-    xml = "application/xml"
-
-
-class DbDialect(StrEnum):
-    postgresql: str = "postgresql"
-    sqlite: str = "sqlite"
-
-
-@dataclass(frozen=True)
-class Destination: ...
-
-
-@dataclass(frozen=True)
-class Source: ...
+type DatabaseType = PostgresDsn | AnyUrl
 
 
 @dataclass(frozen=True)
@@ -179,26 +139,6 @@ class BigqquerySchemaField:
 
 
 @dataclass(frozen=True)
-class BigquerySource(Source):
-    query: str
-    page_size: int | None = None
-
-
-@dataclass(frozen=True)
-class BigqueryDestination(Destination):
-    project: str
-    dataset: str
-    table: str
-    schema: Sequence[BigqquerySchemaField] | None = None
-    write_disposition: Literal[
-        "WRITE_APPEND",
-        "WRITE_TRUNCATE",
-        "WRITE_TRUNCATE_DATA",
-        "WRITE_EMPTY",
-    ] = "WRITE_APPEND"
-
-
-@dataclass(frozen=True)
 class Boto3Credential:
     endpoint_url: HttpUrl
     aws_access_key_id: SecretStr
@@ -219,6 +159,99 @@ class Boto3Scheme(StrEnum):
     s3 = "s3"
 
 
+class CaseInsensitiveStrEnum(StrEnum):
+    @classmethod
+    @validate_call(validate_return=True)
+    def _missing_(cls, value: Any) -> StrEnum | None:  # noqa: ANN401
+        if isinstance(value, str):
+            for member in cls:
+                if member.value == value.lower():
+                    return member
+
+        return None
+
+
+class ContentEncoding(StrEnum):
+    gzip = "gzip"
+
+
+class ContentType(StrEnum):
+    html = "text/html"
+    json = "application/json"
+    svg = "image/svg+xml"
+    xml = "application/xml"
+
+
+class DbDialect(StrEnum):
+    postgresql: str = "postgresql"
+    sqlite: str = "sqlite"
+
+
+@dataclass(frozen=True)
+class DuckDBQuery:
+    query: str
+    alias: str = ""
+    params: object | None = None
+
+
+class Domain:
+    pass
+
+
+class LogLevel(IntEnum):
+    CRITICAL = logging.CRITICAL
+    FATAL = logging.FATAL
+    ERROR = logging.ERROR
+    WARNING = logging.WARNING
+    WARN = logging.WARN  # noqa: LOG009
+    INFO = logging.INFO
+    DEBUG = logging.DEBUG
+    NOTSET = logging.NOTSET
+
+
+@dataclass(frozen=True)
+class PathDomain:
+    file: Path
+    encoding: str | None = None
+    errors: str | None = None
+    _: KW_ONLY
+    is_bytes: bool = False
+
+
+# ================================ Destination & Source ================================
+
+
+@dataclass(frozen=True)
+class Destination: ...
+
+
+@dataclass(frozen=True)
+class Source: ...
+
+
+# --------------------------------------------------------------------------------------
+
+
+@dataclass(frozen=True)
+class BigqueryDestination(Destination):
+    project: str
+    dataset: str
+    table: str
+    schema: Sequence[BigqquerySchemaField] | None = None
+    write_disposition: Literal[
+        "WRITE_APPEND",
+        "WRITE_TRUNCATE",
+        "WRITE_TRUNCATE_DATA",
+        "WRITE_EMPTY",
+    ] = "WRITE_APPEND"
+
+
+@dataclass(frozen=True)
+class BigquerySource(Source):
+    query: str
+    page_size: int | None = None
+
+
 @dataclass(frozen=True)
 class Boto3Destination(Destination, Boto3Domain):
     content_type: ContentType
@@ -228,13 +261,6 @@ class Boto3Destination(Destination, Boto3Domain):
 @dataclass(frozen=True)
 class Boto3Source(Source, Boto3Domain):
     pass
-
-
-@dataclass(frozen=True)
-class DuckDBQuery:
-    query: str
-    alias: str = ""
-    params: object | None = None
 
 
 @dataclass(frozen=True, kw_only=True)
@@ -248,25 +274,6 @@ class DuckDBSource(Source, DuckDBQuery):
 
 
 @dataclass(frozen=True)
-class SQLModelDestination(Destination):
-    model: type[Domain]
-
-
-@dataclass(frozen=True)
-class SQLModelSource(Source):
-    query: Any
-
-
-@dataclass(frozen=True)
-class PathDomain:
-    file: Path
-    encoding: str | None = None
-    errors: str | None = None
-    _: KW_ONLY
-    is_bytes: bool = False
-
-
-@dataclass(frozen=True)
 class PathDestination(Destination, PathDomain):
     newline: str | None = None
 
@@ -274,6 +281,16 @@ class PathDestination(Destination, PathDomain):
 @dataclass(frozen=True)
 class PathSource(Source, PathDomain):
     pass
+
+
+@dataclass(frozen=True)
+class SQLModelDestination(Destination):
+    model: type[Domain]
+
+
+@dataclass(frozen=True)
+class SQLModelSource(Source):
+    query: Any
 
 
 @dataclass(config=CONFIG_DICT, frozen=True)
