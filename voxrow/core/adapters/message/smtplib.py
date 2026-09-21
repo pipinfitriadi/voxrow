c4ed6fcd5aedbad6c9838bdddc97c49a01d9e41b
Pipin Fitriadi <pipin.fitriadi@alto.id>
2026-09-21 15:37:42 +0700

    feat: Init SmtpMessageAdapter

---
+++ voxrow/core/adapters/message/smtplib.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+# Copyright (C) Pipin Fitriadi - All Rights Reserved
+
+# Unauthorized copying of this file, via any medium is strictly prohibited
+# Proprietary and confidential
+# Written by Pipin Fitriadi <pipinfitriadi@gmail.com>, 21 September 2026
+
+from . import AbstractMessagePort
+
+
+class SmtpMessageAdapter(AbstractMessagePort):
+    pass
